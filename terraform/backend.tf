bec9cc6838f551d4d94d5d687dac366f3931711e
MzJennie <talkaboutem@gmail.com>
2026-06-07 08:23:07 +0000

    Add DynamoDB state locking and update documentation

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "project-bedrock-tfstate-3635"
-    key    = "production/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "project-bedrock-tfstate-3635"
+    key            = "production/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
   }
 }
