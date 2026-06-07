43da26597cd8b467d4c2368bf321ba95d71a67b1
MzJennie <talkaboutem@gmail.com>
2026-06-07 09:36:01 +0000

    Add grading.json and final deployment files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -32,21 +32,3 @@ output "postgres_endpoint" {
   description = "PostgreSQL RDS endpoint"
   value       = module.rds.postgres_endpoint
 }
-
-output "dev_view_access_key_id" {
-  description = "Developer IAM user access key"
-  value       = module.iam.dev_view_access_key_id
-  sensitive   = true
-}
-
-output "dev_view_secret_access_key" {
-  description = "Developer IAM user secret key"
-  value       = module.iam.dev_view_secret_access_key
-  sensitive   = true
-}
-
-output "dev_view_password" {
-  description = "Developer IAM user console password"
-  value       = module.iam.dev_view_password
-  sensitive   = true
-}
